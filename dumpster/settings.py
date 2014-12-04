39541ba862e5a7c5ea1e54132f1698ffa526d5ce
Tom Zierbock <tomzierbock@gmail.com>
2014-12-04 21:17:13 +0000

    Ready for heroku deployment

--- dumpster/settings.py
+++ dumpster/settings.py
@@ -81,6 +81,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+#STATIC_URL = '/static/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#MEDIA_URL = 'media/'
+
+
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = 'media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
